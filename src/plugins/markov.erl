a6984cb97bc32b35836546059c6e61084d99c4a9
mbal <matteo.bana@gmail.com>
2013-02-16 14:43:08 +0100

    Added talk plugin
    Still not very stable, though. First !bot talk init <order>, and !bot
    talk <length>. It needs a text file to train. The file name is specified
    in settings.cfg

---
+++ src/plugins/markov.erl
@@ -0,0 +1,73 @@
+-module(markov).
+-export([train/3, generate_text/2]).
+
+train(FileName, Order, Table) ->
+    {ok, Co} = file:open(FileName, [read]),
+    parse_file(Co, Order, Table, []).
+
+parse_file(FileObj, Order, Table, RemWords) ->
+    Line = file:read_line(FileObj),
+    case Line of
+        eof -> Table;
+        {ok, Data} -> 
+            {Tab2, RemainingWords} = parse_line(string:to_lower(Data), 
+                                                Order, Table, RemWords),
+            parse_file(FileObj, Order, Tab2, RemainingWords);
+        Error -> io:format("~p~n", [Error]),
+                 Error
+    end.
+
+parse_line(Data, Order, Table, RemWords) ->
+    case string:tokens(Data, "\r\n") of
+        [] -> {Table, RemWords};
+        Line ->
+            StripLine = lists:nth(1, Line),
+            Words = string:tokens(StripLine, " "),
+            insert_ngram(RemWords ++ Words, Order, Table)
+    end.
+
+insert_ngram([_|Rest]=Words, Order, Table) ->
+    case length(Words) > Order of
+        true ->
+            Pre = lists:sublist(Words, Order),
+            Post = lists:nth(Order+1, Words),
+            Table2 = dict:update(Pre, fun(Old) ->
+                                              [Post | Old] end, 
+                                 [Post], Table),
+            insert_ngram(Rest, Order, Table2);
+        false ->
+            %%not always the line ends with the right number
+            %%of words; so we must return the remaining words to
+            %% the caller, which will pass them to parse_line
+            {Table, Words}
+    end.
+
+generate_text(Length, Table) ->
+    generate_text(Length, Table, random_state(Table)).
+
+generate_text(Length, Table, InitialState) ->
+    string:join(generate_text_aux(Length, Table, InitialState, []), " ").
+
+generate_text_aux(Length, Table, [_|Rest]=PrevState, Acc) ->
+    case length(Acc) > Length of
+        true ->
+            io:format("end~n"),
+            lists:reverse(Acc);
+        false ->
+            case dict:find(PrevState, Table) of
+                {ok, ListProbNextWords} ->
+                    NextWord = random_choice(ListProbNextWords),
+                    io:format("~p~n", [NextWord]),
+                    generate_text_aux(Length, Table, Rest ++ [NextWord], [NextWord | Acc]);
+                error -> 
+                    io:format("no such key in dict ~p~n", [PrevState]),
+                    generate_text_aux(Length, Table, random_state(Table), Acc)
+            end
+    end.
+
+
+random_choice(List) ->
+    lists:nth(random:uniform(length(List)), List).
+
+random_state(Dictionary) ->
+    random_choice(dict:fetch_keys(Dictionary)).
---
+++ src/plugins/talk.erl
@@ -0,0 +1,90 @@
+%%%-------------------------------------------------------------------
+%%% @author mbal 
+%%% @copyright (C) 2013, 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15 Feb 2013 by  mbal
+%%%-------------------------------------------------------------------
+-module(talk).
+
+-behaviour(gen_event).
+
+%% gen_event callbacks
+-export([init/1, handle_event/2, handle_call/2, 
+         handle_info/2, terminate/2, code_change/3]).
+
+-export([name/0, short_description/0]).
+
+name() ->
+    "talk".
+short_description() ->
+    "generate some text through a Markov chain".
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {table}).
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_event({cmd, _, "talk", ["init", K]}, State) ->
+    {Order, _} = string:to_integer(K),
+    case (Order < 1) or (Order > 4) of
+        true -> 
+            plugin_api:send_priv_msg("Order isn't in [1, 4]"),
+            NewState = #state{table=dict:new()};
+        false ->
+            NewState = #state{table=start_training(Order, State)}
+    end,
+    {ok, NewState};
+handle_event({cmd, _, "talk", ["init"]}, State) ->
+    NewState = start_training(3, State),
+    {ok, NewState};
+
+%%handle_event({cmd, _, "talk", ["delete"]}, State) ->
+%%    {ok, #state{table=dict:new()}};
+
+handle_event({cmd, _, "talk", [Len]}, State) ->
+    {Length, _} = string:to_integer(Len),
+    case (Length < 10) or (Length > 300) of
+        true ->
+            plugin_api:send_priv_msg("Length isn't in [10, 300]!");
+        false ->
+            plugin_api:send_priv_msg(generate_text(Length, State#state.table))
+    end,
+    {ok, State};
+
+handle_event({cmd, _, "talk", []}, State) ->
+    plugin_api:send_priv_msg(generate_text(100, State#state.table)),
+    {ok, State};
+
+handle_event(_Event, State) ->
+    {ok, State}.
+
+handle_call(_Request, State) ->
+    Reply = ok,
+    {ok, Reply, State}.
+
+handle_info(_Info, State) ->
+    {ok, State}.
+
+terminate(_Reason, State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+start_training(Order, State) ->
+    plugin_api:send_priv_msg("Starting training..."),
+    TrainFileName = conf_server:lookup(train_file),
+    Tab = markov:train(TrainFileName, Order, dict:new()),
+    plugin_api:send_priv_msg("Training finished. Now you can use talk <length>!"),
+    Tab.
+
+generate_text(Length, Tab) ->
+    markov:generate_text(Length, Tab).
