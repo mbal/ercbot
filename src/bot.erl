6139a59092dc04f76fa033a9e4cae33b7968e848
mbal <matteo.bana@gmail.com>
2013-02-17 16:57:33 +0100

    new way to start application
    Added new way to start the application directly via command line.
    (documented also in readme)

--- src/bot.erl
+++ src/bot.erl
@@ -1,5 +1,21 @@
 -module(bot).
--export([start/1, stop/0]).
+-export([start/0, start/1, stop/0]).
+
+%%% start/0 should be called only if the settings_file environment variable
+%%% was set (in a .config file or passed in the command line)
+%%%
+%%% $ cat start_conf.config
+%%%  [{bot, [{settings_file, "path/to/settings.cfg"}]}].
+%%% $ erl -pa ebin -config start_conf
+%%%
+%%% or
+%%%
+%%% $ erl -bot settings_file '"path/to/settings.cfg"' -pa ebin
+%%% (note that there are both ' and ").
+%%% and then, in both cases, bot:start().
+
+start() ->
+    application:start(bot).
 
 start(SettingsFile) ->
     application:set_env(bot, settings_file, SettingsFile),
