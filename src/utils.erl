d0fde1ac53f96bf1a8547352c33931bcaed13d6e
mbal <matteo.bana@gmail.com>
2013-02-19 13:27:06 +0100

    CTCP fix

--- src/utils.erl
+++ src/utils.erl
@@ -6,12 +6,17 @@ irc_parse(Data) ->
     tokens_parse(Tok).
 
 tokens_parse([User, "PRIVMSG", _Channel | Rest]) ->
+    %% right now, Channel isn't really important, since the
+    %% bot can listen on a single channel.
     FirstWord = lists:nth(1, Rest),
     CmdString = conf_server:lookup(cmd_string),
     Nick = lists:nth(1, string:tokens(User, "!")),
     case CmdString == FirstWord of
-        true -> parse_cmd(Nick, tl(Rest));
-        false -> %%at this point the message could be a CTCP message
+        true -> 
+            parse_cmd(Nick, tl(Rest));
+        false -> 
+            %% at this point the message could either be a 
+            %% CTCP message or a real PRIVMSG
             Message = string:join(Rest, " "),
             case ctcp_parse(Message) of
                 {Command, Data} -> {ctcp, Command, Data};
@@ -24,8 +29,10 @@ tokens_parse([User, "QUIT", _Channel]) ->
     {control, user_quit, User};
 tokens_parse([User, "JOIN", _Channel]) ->
     {control, user_join, User};
+
 tokens_parse([_, "353", _, _, Channel | UserList]) ->
     {control, user_list, Channel, UserList};
+
 tokens_parse([_, "376" | _]) ->
     {control, join};
 tokens_parse([_, "433" | _]) ->
@@ -36,15 +43,13 @@ tokens_parse(_) ->
     ok.
 
 ctcp_parse(Cmd) ->
-    %%I can't seem to get a pattern matching in the function 
-    %%definition that is nice and always works. So:
-    %%fuck it, I'll use a regular expression!
+    %% I can't seem to get a nice pattern for the function 
+    %% definition that always works, so fuck it, 
+    %% I'll use a regular expression!
     case re:run(Cmd, "^\x01([a-zA-Z]*) ?([a-zA-Z0-9]*)?\x01$",
                 [global, {capture, all_but_first, list}]) of
         nomatch -> false;
-        %%{match, [[FirstG, SecondG]]}
         {match, [[FirstG, SecondG]]} -> {FirstG, SecondG};
-        What -> io:format("~w", [What])
     end.
 
 parse_cmd(Nick, [Cmd|Args]) ->
