f08e619a630cb722e756787c1f2683b8fdc2c19b
mbal <matteo.bana@gmail.com>
2013-02-19 12:19:44 +0100

    parse CTCP messages
    added parsing for ctcp messages.

--- src/utils.erl
+++ src/utils.erl
@@ -5,14 +5,25 @@ irc_parse(Data) ->
     Tok = string:tokens(Data, ": "),
     tokens_parse(Tok).
 
-tokens_parse([User, "PRIVMSG", Channel | Rest]) ->
+tokens_parse([User, "PRIVMSG", _Channel | Rest]) ->
     FirstWord = lists:nth(1, Rest),
     CmdString = conf_server:lookup(cmd_string),
     Nick = lists:nth(1, string:tokens(User, "!")),
     case CmdString == FirstWord of
         true -> parse_cmd(Nick, tl(Rest));
-        false -> {priv_msg, string:join(Rest, " ")} 
+        false -> %%at this point the message could be a CTCP message
+            Message = string:join(Rest, " "),
+            case ctcp_parse(Message) of
+                {Command, Data} -> {ctcp, Command, Data};
+                false -> {priv_msg, Message}
+            end
     end;
+tokens_parse([User, "PART", _Channel]) ->
+    {control, user_quit, User};
+tokens_parse([User, "QUIT", _Channel]) ->
+    {control, user_quit, User};
+tokens_parse([User, "JOIN", _Channel]) ->
+    {control, user_join, User};
 tokens_parse([_, "353", _, _, Channel | UserList]) ->
     {control, user_list, Channel, UserList};
 tokens_parse([_, "376" | _]) ->
@@ -23,6 +34,18 @@ tokens_parse(["PING" | Rest]) ->
     {control, ping, Rest};
 tokens_parse(_) ->
     ok.
+
+ctcp_parse(Cmd) ->
+    %%I can't seem to get a pattern matching in the function 
+    %%definition that is nice and always works. So:
+    %%fuck it, I'll use a regular expression!
+    case re:run(Cmd, "^\x01([a-zA-Z]*) ?([a-zA-Z0-9]*)?\x01$",
+                [global, {capture, all_but_first, list}]) of
+        nomatch -> false;
+        %%{match, [[FirstG, SecondG]]}
+        {match, [[FirstG, SecondG]]} -> {FirstG, SecondG};
+        What -> io:format("~w", [What])
+    end.
 
 parse_cmd(Nick, [Cmd|Args]) ->
     Command = lists:nth(1, string:tokens(Cmd, "\r\n")),
