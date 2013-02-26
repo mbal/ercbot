8b980562547ea6cb79b9e518de1189e56a611c93
mbal <matteo.bana@gmail.com>
2013-02-26 17:51:14 +0100

    admin is a gen_server

--- src/plugins/admin.erl
+++ src/plugins/admin.erl
@@ -1,12 +1,39 @@
+%%%-------------------------------------------------------------------
+%%% @author  mbal
+%%% @copyright (C) 2013, 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 26 Feb 2013 by  mbal
+%%%-------------------------------------------------------------------
 -module(admin).
 
--behaviour(gen_event).
--record(state, {admins}).
+-behaviour(gen_server).
 
--export([init/1, handle_event/2, terminate/2, handle_call/2, handle_info/2, code_change/3]).
--export([name/0]).
+%% API
+-export([start_link/0, name/0]).
 
-name() -> none.
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {admin_list}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+name() ->
+    none.
+
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
 
 init([]) ->
     case ets:lookup(state_storage, ?MODULE) of
@@ -14,63 +41,76 @@ init([]) ->
             {ok, StateData};
         [] ->
             Admins = conf_server:lookup(admin),
-            {ok, #state{admins=Admins}}
+            {ok, #state{admin_list=Admins}}
     end.
 
-handle_event({cmd, Channel, Nick, "admin", Args}, State) ->
-    Res = case lists:member(Nick, State#state.admins) of
-              true ->
-                  handle_command(State, Channel, Args);
-              false ->
-                  irc_api:send_priv_msg(Channel, 
-                                        "You're not on my list, sorry"),
-                  State
-          end,
-    {ok, Res};
-handle_event(_Req, State) ->
-    {ok, State}.
+handle_call(_Request, _From, State) ->
+    {reply, ok, State}.
 
-handle_call(_Request, State) -> {ok, ok, State}.
-handle_info(_Info, State) -> {ok, State}.
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
-terminate(_Args, State) -> 
-    ets:insert(state_storage, {admin, State}),
+handle_cast({cmd, Channel, Nick, "admin", Args}, State) ->
+    NewState = case lists:member(Nick, State#state.admin_list) of
+                   true ->
+                       handle_command(Args, Channel, State);
+                   false ->
+                       irc_api:send_priv_msg(Channel, "You're not on my list"),
+                       State
+    end,
+    {noreply, NewState};
+handle_cast(_Cast, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, State) ->
+    ets:insert(state_storage, {?MODULE, State}),
     ok.
 
-get_admins(State) -> string:join(State#state.admins, ", ").
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
 
-handle_command(State, Channel, Args) when length(Args) =< 2 ->
-    case Args of
-        ["add", User] ->
-            NAdminList = [User|State#state.admins],
-            conf_server:update(admin, NAdminList),
-            State#state{admins=NAdminList};
-        ["rem", User] ->
-            NAdminList = lists:delete(User, State#state.admins),
-            conf_server:update(admin, NAdminList),
-            State#state{admins=NAdminList};
-        ["list"] ->
-            irc_api:send_priv_msg(Channel, get_admins(State)), 
-            State;
-        ["cnick", NewNick] ->
-            irc_api:change_nick(NewNick),
-            State;
-        ["restart"] ->
-            irc_api:send_priv_msg(Channel, "Restarting"),
-            irc_api:restart_bot(), 
-            State;
-        ["shutdown"] ->
-            irc_api:send_priv_msg(Channel, "Goodbye, suckers!"),
-            irc_api:shutdown_bot(), 
-            State;
-        ["crash"] ->
-            _ = 1/0,
-            State;
-        _ ->
-            irc_api:send_priv_msg(Channel, 
-                                  "Unrecognized or incomplete option"),
-            State
-    end;
-handle_command(State, Channel, _Args) ->
-    plugin_mgr_api:send_priv_msg(Channel, "Too many options"),
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+handle_command(["add", User], _Channel, State) ->
+    NewAdminList = [User | State#state.admin_list],
+    conf_server:update(admin, NewAdminList),
+    State#state{admin_list=NewAdminList};
+handle_command(["rem", User], _Channel, State) ->
+    NewAdminList = lists:delete(User, State#state.admin_list),
+    conf_server:update(admin, NewAdminList),
+    State#state{admin_list=NewAdminList};
+handle_command(["list"], Channel, State) ->
+    irc_api:send_priv_msg(Channel, get_admins(State#state.admin_list)),
+    State;
+handle_command(["cnick", NewNick], _Channel, State) ->
+    irc_api:change_nick(NewNick),
+    State;
+handle_command(["restart"], Channel, State) ->
+    irc_api:send_priv_msg(Channel, "I'll be back!"),
+    irc_api:restart_bot(),
+    State;
+handle_command(["join", ChannelName], _Channel, State) ->
+    case lists:nth(1, ChannelName) == $# of
+        true ->
+            irc_api:join_channel(ChannelName);
+        false ->
+            irc_api:join_channel([$# | ChannelName])
+    end,
+    State;
+handle_command(["leave", ChannelName], _Channel, State) ->
+    case lists:nth(1, ChannelName) == $# of
+        true ->
+            irc_api:leave_channel(ChannelName);
+        false ->
+            irc_api:leave_channel([$# | ChannelName])
+    end,
+    State;
+handle_command(["shutdown"], _Channel, State) ->
+    irc_api:shutdown_bot(),
+    State;
+handle_command(_, Channel, State) ->
+    irc_api:send_priv_msg(Channel, "Unrecognized or incomplete option"),
     State.
+
+get_admins(AdminList) -> string:join(AdminList, ", ").
