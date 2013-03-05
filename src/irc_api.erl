a2afc84c34cb71660bf6b2799fdc8277c945865b
mbal <matteo.bana@gmail.com>
2013-03-05 20:06:06 +0100

    added utility, reorganized export

--- src/irc_api.erl
+++ src/irc_api.erl
@@ -1,8 +1,15 @@
 -module(irc_api).
+
+%% main irc commands
 -export([send_priv_msg/2, change_nick/1, send_msg/1]).
--export([shutdown_bot/0, restart_bot/0, reload_plugins/0]).
--export([load_plugin/1, remove_plugin/1, is_admin/1]).
+%% Bot related
+-export([shutdown_bot/0, restart_bot/0]).
+%% plugins managing
+-export([load_plugin/1, remove_plugin/1, reload_plugins/0]).
+%% control
 -export([join_channel/1, leave_channel/1]).
+%% misc utilities
+-export([is_admin/1, is_channel/1]).
 
 -define(BOT, bot_fsm).
 
@@ -41,6 +48,10 @@ load_plugin(PluginName) ->
 remove_plugin(PluginName) ->
     gen_server:call(plugin_mgr, {remove, PluginName}).
 
+%% is_admin(string()) -> true | false.
 is_admin(Name) ->
     lists:member(Name, conf_server:lookup(admin)).
 
+%% is_channel(string()) -> true | false.
+is_channel([$# | _Rest]) -> true;
+is_channel(_)            -> false.
